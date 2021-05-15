5e1e88a454f581300878c88af7f8230f3abb8a89
EzequielDelaloye <80924007+EzequielDelaloye@users.noreply.github.com>
2021-05-14 22:17:54 -0300

    Add files via upload

--- ACTIVIDAD2.py
+++ ACTIVIDAD2.py
@@ -22,15 +22,19 @@ while(urlbase['next'] is not None):
     urlbase = consultar_personajes(urlbase['next'])
 
 
+def nombre (item):
+    '''sirve para ordenar por sublistas convertidas a numeros en este caso la altura '''
+    return (item['name'])
 
-def altura (item):
-        return (item['name'])
-
-sw_data.sort(key=altura)
+sw_data.sort(key=nombre)
 
 for index, character in enumerate(sw_data):
     
-    print(character['name'], character['species'], character['homeworld'])
+    print(character['name'],'su raza es: ', character['species'],'Su planeta de origen es :', character['homeworld'])
 
 
 
+    
+for character in sw_data:
+    if(len(character['films'])== 6):
+        print('El personaje',character['name'],'aparecio en 6 peliculas')
